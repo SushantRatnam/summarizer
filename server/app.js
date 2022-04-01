600dca93daacf337ab58b7fb6e36306f6ad30a53
Sushant Ratnam <sushantr@zeta.tech>
2022-04-02 01:16:02 +0530

    youtube to summarized text api created

--- server/app.js
+++ server/app.js
@@ -1,10 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import 'dotenv/config';
-
-import deepai from 'deepai';
-import { convertToAudio } from './utils.js';
-import { v4 as uuidv4 } from 'uuid';
+import { converToSummarizedText } from './utils.js';
 
 //app config
 const app = express();
@@ -15,77 +12,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 const PORT = process.env.PORT || 3000;
-const DG_KEY = process.env.DG_KEY;
-const DEEP_AI_KEY = process.env.DEEP_AI_KEY;
-
-if (DG_KEY === undefined) {
-  throw 'You must define DG_KEY in your .env file';
-}
-
-
-
-const deepgram = new Deepgram(DG_KEY);
-deepai.setApiKey(DEEP_AI_KEY);
-let source;
-// if (file.startsWith('http')) {
-//   // File is remote
-//   // Set the source
-//   source = {
-//     url: file,
-//   };
-// } else {
-//   // File is local
-//   // Open the audio file
-//   const audio = fs.readFileSync(file);
-
-//   // Set the source
-//   source = {
-//     buffer: audio,
-//     mimetype: mimetype,
-//   };
-// }
-
-// deepgram.transcription
-//   .preRecorded(source, {
-//     punctuate: true,
-//   })
-//   .then((response) => {
-//     // Write the response to the console
-//     // console.dir(response, { depth: null });
-
-//     // Write only the transcript to the console
-//     const text = response.results.channels[0].alternatives[0].transcript;
-//     // console.log(text);
-//     try {
-//       fs.writeFileSync('test/full.txt', text);
-//     } catch (error) {
-//       console.log(error);
-//     }
-//     (async function () {
-//       var resp = await deepai.callStandardApi('summarization', {
-//         text,
-//       });
-//         try {
-//           console.dir(resp)
-//         fs.writeFileSync('test/summarized.txt', resp.output);
-//       } catch (error) {
-//         console.log(error);
-//       }
-//     })();
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
-// convertToAudio().then((e)=> console.log(e) )
 
 app.get('/uri', (req, res) => {
   (async function () {
     console.log('req.query', req.query);
     try {
-      const videoId = uuidv4();
-      await convertToAudio(req.query.youtubeUrl, videoId);
-      
-      res.json(response);
+      const summarizedText = await converToSummarizedText(req.query.youtubeUrl);
+      res.json({
+        summarizedText
+      })
     } catch (error) {
       console.log(error);
     }
--- server/utils.js
+++ server/utils.js
@@ -1,18 +1,73 @@
+import deepai from 'deepai';
 import ytdl from 'ytdl-core';
 import * as fs from 'fs';
-import path from 'path';
+import path, { format } from 'path';
 import { Deepgram } from '@deepgram/sdk';
+import ffmpeg from 'fluent-ffmpeg';
 
-export async function convertToAudio(url, videoId) {
-  ytdl(decodeURIComponent(url), {
-    filter: (format) => format.itag === 140,
-    quality: 'highestaudio',
-  }).pipe(fs.createWriteStream(path.join(path.resolve('./'), `store/output/${videoId}.mp3`)));
-}
+const DEEP_AI_KEY = process.env.DEEP_AI_KEY;
+const DG_KEY = process.env.DG_KEY;
+deepai.setApiKey(DEEP_AI_KEY);
 
-export function convertToText(mp3Path) {
+export async function convertToText(location, videoId) {
+  if (DG_KEY === undefined) {
+    throw 'You must define DG_KEY in your .env file';
+  }
   const mimetype = 'audio/mpeg';
   const deepgram = new Deepgram(DG_KEY);
+
+  let source;
+  const audio = fs.readFileSync(location);
+  source = {
+    buffer: audio,
+    mimetype: mimetype,
+  };
+
+  const response = await deepgram.transcription
+    .preRecorded(source, {
+      punctuate: true,
+    })
+    .catch((err) => {
+      console.log('error ======', err);
+    });
+  const text = response.results.channels[0].alternatives[0].transcript;
+  const resp = await deepai.callStandardApi('summarization', { text });
+  return resp;
 }
 
-export function summarizeText() {}
+const ffmpegSync = (stream, storage_location) => {
+  return new Promise((resolve, reject) => {
+    ffmpeg(stream)
+      .audioBitrate(128)
+      .withAudioCodec('libmp3lame')
+      .toFormat('mp3')
+      .saveToFile(storage_location)
+      .on('error', function (err) {
+        console.log('error', err);
+        return reject(new Error(err));
+      })
+      .on('end', function () {
+        resolve({ success: true });
+      });
+  });
+};
+
+export const converToSummarizedText = async (link) => {
+  const info = await ytdl.getInfo(link, { quality: 'highestaudio' });
+  const converted_folder = path.join(path.resolve('./'), `store/audio`);
+  if (!fs.existsSync(converted_folder)) {
+    fs.mkdirSync(converted_folder);
+  }
+  const meta = info['player_response']['videoDetails'];
+  const storage_location = path.join(converted_folder, `${meta['videoId']}.mp3`);
+
+  // downloading and converting to mp3
+  const stream = ytdl.downloadFromInfo(info, {
+    quality: 'highestaudio',
+  });
+  const response = await ffmpegSync(stream, storage_location);
+  if (response.success === true) {
+    const text = await convertToText(storage_location, meta['videoId']);
+    return text;
+  }
+};
